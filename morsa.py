921dbea550eb9e84bafc3cb9c58c07dd1b44f629
Ferran <ferran.obiols.colomina@gmail.com>
2018-03-07 00:08:17 +0100

    base for CLI

--- morsa.py
+++ morsa.py
@@ -7,6 +7,11 @@ from prompt_toolkit.contrib.completers import WordCompleter
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 from prompt_toolkit.history import InMemoryHistory, FileHistory
 
+PROJECT_OPTIONS = ['exit', 'help', 'use', 'create', 'open', 'info', 'sethistory', 'setname', 'setpath']
+
+def print_help():
+    print('TODO!')
+
 def print_logo():
     print("""
             .-9 9 `\\
@@ -25,30 +30,48 @@ def print_logo():
                    `.__/
     """)
 
-def update_toolbar(cli): #TODO: dynamic toolbar
-    return [(Token.Toolbar, 'Toolbar text1')]
+def update_toolbar(cli, title='Temporal project [/tmp/morsa/]'):
+    return [(Token.Toolbar, title)]
 
 def main():
-    #TODO: change logo for screen h/w
-    #print_logo()
+    #TODO: print the logo depending on screen resolution
 
-    #history = InMemoryHistory()
-    history=FileHistory('/tmp/morsa_history.txt')
-    suggest=AutoSuggestFromHistory()
-    words = WordCompleter(['<html>', '<body>', '<head>', '<title>'], ignore_case=True)
+    #TODO: load all modules
+    
+    #Temporal project variables
+    history = InMemoryHistory()    
+    suggest = AutoSuggestFromHistory()    
 
+    words = WordCompleter(PROJECT_OPTIONS, ignore_case=True)
+
+    module = None
     end = False
     while not end:
-        user_input = prompt('> ', 
-            completer=words, 
-            history=history, 
-            auto_suggest=suggest, 
-            get_bottom_toolbar_tokens=update_toolbar)
+        user_input = prompt(
+            '({0})> '.format(module) if module else '> ',
+            get_bottom_toolbar_tokens=update_toolbar,
+            auto_suggest=suggest,
+            completer=words,
+            history=history
+        )
         
-        if user_input in ('exit', 'quit'):
+        if user_input == 'help':
+            print_help()
+
+        elif user_input == 'exit':
             end = True
+        
+        elif 'sethistory' in user_input:
+            path = user_input.split(' ')[2]
+            print('[+] New history file: {0}'.format(path))
+            history = FileHistory(path)
+
+        elif 'use' in user_input:
+            module = user_input.split(' ')[1]
+            #TODO: send input to the module
+        
         else:
-            print('[+] Nope: {0}'.format(user_input))
+            print('[-] Option "{0}" not found. Please use "help" to see the correct options.'.format(user_input))
 
 if __name__ == '__main__':
     main()
